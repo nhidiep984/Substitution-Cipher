e7bdb71f1162527eb75ec1308db760985eb09f98
nhidiep984 <48866391+nhidiep984@users.noreply.github.com>
2020-03-05 18:39:28 -0500

    'got rid of global variables'

--- cipher_program.py
+++ cipher_program.py
@@ -2,27 +2,27 @@ from string import ascii_lowercase
 import random
 
 def main():
-  key_gen()
+  key = key_gen()
   message = input("Enter message to encrypt: ").lower()
-  ciphertext = encryption(message)
+  ciphertext = encryption(message, key)
   print(f"Ciphertext: {ciphertext}")
   print(f"Key: {key}")
   ciphertext = input("Enter ciphertext to decrypt: ")
-  decrypted_message = decryption(ciphertext)
+  decrypted_message = decryption(ciphertext, key)
   print(f"Decrypted message: {decrypted_message}")
 
 def key_gen():
-  global key
   letters = 'abcdefghijklmnopqrstuvwxyz'
   key = list(letters)
   random.shuffle(key)
-  key = ''.join(key)
+  return ''.join(key)
 
-def encryption(message):
+
+def encryption(message, key):
   translation = str.maketrans(ascii_lowercase, key)
   return message.translate(translation)
 
-def decryption(ciphertext):
+def decryption(ciphertext, key):
   translation = str.maketrans(key, ascii_lowercase)
   return ciphertext.translate(translation)
 
